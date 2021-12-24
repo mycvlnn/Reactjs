eb3c323d7146587eb547a0ad3de1c05bd56cc4eb
Le Ngoai Ngu <ngunl@capichiapp.com>
2021-12-24 21:22:12 +0700

    push v1

---
+++ src/components/FormInput.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./formInput.css"
+export default function FormInput() {
+  return (
+    <div className="formInput">
+      <label htmlFor="">User name</label>
+      <input type="text" />
+    </div>
+  )
+}

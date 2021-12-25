a94d69772a2248c827f3d28b6003318a546511a7
Le Ngoai Ngu <ngunl@capichiapp.com>
2021-12-25 07:56:55 +0700

    validation not library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,88 @@
+import { useState } from "react"
 import FormInput from "./components/FormInput"
 
 function App() {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  })
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special character !",
+      label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "email",
+      errorMessage: "It should be a valid email address!",
+      label: "email",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "date",
+      placeholder: "Birthday",
+      label: "Birthday",
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage:
+        "Password should be 8-25 characters and include at least letter, 1 number, and 1 spcial characters",
+      label: "Password",
+      pattern: "^[A-Za-z0-9]{8,25}$",
+      required: true,
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      errorMessage: "Passwords don't match!",
+      label: "Confirm Password",
+      pattern: values.password,
+      required: true,
+    },
+  ]
+  const handleChange = (e) => {
+    const { value, name } = e.target
+    setValues({
+      ...values,
+      [name]: value,
+    })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    alert("Submit successfully !")
+  }
   return (
     <div className="App">
-      <form>
-        <FormInput placeholder="User name" />
-        <FormInput placeholder="Email" />
-        <FormInput placeholder="Full name" />
-        <FormInput placeholder="Sth else" />
+      <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            onChange={handleChange}
+            value={values[input.name]}
+          />
+        ))}
+        <button>Submit</button>
       </form>
     </div>
   )
--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,10 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 import "./formInput.css"
-export default function FormInput() {
+export default function FormInput(props) {
+  const [focused, setFocused] = useState(false)
+  const { id, label, errorMessage, onChange, ...inputProps } = props
+  const handleFocus = (e) => {
+    setFocused(true)
+  }
   return (
     <div className="formInput">
-      <label htmlFor="">User name</label>
-      <input type="text" />
+      <label htmlFor={id}>{label}</label>
+      <input
+        id={id}
+        onChange={onChange}
+        {...inputProps}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span className="text-errorMessage">{errorMessage}</span>
     </div>
   )
 }
